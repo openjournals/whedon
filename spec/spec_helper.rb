99885dd20012f8d9bbf516d3079cc02ba45e9f1c
Arfon Smith <arfon.smith@gmail.com>
2016-05-13 17:47:51 -0600

    Removing tokens

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require "vcr"
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.filter_sensitive_data("<GITHUB_TOKEN>") { ENV['GH_TOKEN'] }
 end
 
 require_relative "../lib/whedon"
