242f0087392dad5ae605828b9960d84cbb877277
Arfon Smith <arfon.smith@gmail.com>
2016-05-13 17:49:46 -0600

    Simple spec for Review class

---
+++ spec/review_spec.rb
@@ -0,0 +1,12 @@
+require_relative 'spec_helper'
+
+describe Whedon do
+
+  subject { Whedon::Review.new(17, ENV['JOSS_REVIEW_REPO']) }
+
+  it "can should return the correct number of review issues" do
+    VCR.use_cassette('review') do
+      expect(subject.issue_body).to match(/Submitting author/)
+    end
+  end
+end
