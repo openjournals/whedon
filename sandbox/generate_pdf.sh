3700eefde7df64284c95f336b2510523a1cc0f4f
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-10-25 23:54:50 -0400

    working draft of static citation input

--- sandbox/generate_pdf.sh
+++ sandbox/generate_pdf.sh
@@ -8,9 +8,9 @@ review_issue_url=$6
 bibfile=$7
 filename_doi="output.pdf" #use static name for sake of testing
 latex_template_path="../resources/joss.template"
-#mytext_citation="Attali, D. (2016, September 20). ezknitr: Avoid the Typical Working Directory Pain When Using knitr The Journal of Open Source Software. The Open Journal. https://doi.org/10.21105/joss.00075"
-mytext_citation=$(curl "http://citation.crosscite.org/format?doi=10.21105/joss.00075&style=apa&lang=en-US")
-mytext_citation2=`cat $mytext_citation`
+mytext_citation="Attali, D. (2016, September 20). ezknitr: Avoid the Typical Working Directory Pain When Using knitr The Journal of Open Source Software. The Open Journal. https://doi.org/10.21105/joss.00075"
+#mytext_citation=$(curl "http://citation.crosscite.org/format?doi=10.21105/joss.00075&style=apa&lang=en-US")
+mytext_citation2=`echo $mytext_citation`
 
       # TODO: may eventually want to swap out the latex template
       cd $paper_directory && pandoc \
@@ -18,7 +18,8 @@ mytext_citation2=`cat $mytext_citation`
       -V archive_doi=$archive_doi \
       -V paper_url=$paper_url \
       -V formatted_doi=$formatted_doi \
-      -V text_citation=$mytext_citation2 \
+      -V text_citation="$mytext_citation2" \
+      -V graphics="true" \
       -V review_issue_url=$review_issue_url \
       -S -o $filename_doi -V geometry:margin=1in \
       --filter pandoc-citeproc $bibfile \
