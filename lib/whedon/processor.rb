632272ef91f0719b818efd3e4f4f964d525959cb
Arfon Smith <arfon.smith@gmail.com>
2017-11-13 14:35:21 -0500

    Success

--- lib/whedon/processor.rb
+++ lib/whedon/processor.rb
@@ -117,7 +117,7 @@ module Whedon
       --template #{latex_template_path}`
 
       if File.exists?("#{paper.directory}/#{paper.filename_doi}.pdf")
-        `open #{paper.directory}/#{paper.filename_doi}.pdf`
+        puts "Paper compilation successful!"
       else
         puts "Looks like we failed to compile the PDF"
       end
