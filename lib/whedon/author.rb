4bc3ad988bea444753fd621b6ca2fe2b915440d8
Arfon Smith <arfon.smith@gmail.com>
2017-12-19 06:38:47 -0500

    Mixed-type affiliations

--- lib/whedon/author.rb
+++ lib/whedon/author.rb
@@ -17,14 +17,18 @@ module Whedon
       # Some people have two affiliations, if this is the case then we need
       # to parse each one and build the affiliation string.
       author_affiliations = []
-    
+
       return nil if index.nil? # Some authors don't have an affiliation
 
       affiliations = if index.to_s.include?(',')
-        index.split(',').map { |a| a.to_i }
+        index.split(',').map { |a| a.strip }
       else
-        [ index.to_i ]
+        [ index.to_s ]
       end
+
+      # We need to turn all of the affiliation YAML keys into strings
+      # So that mixed integer and string affiliations work
+      affiliations_yaml = affiliations_yaml.inject({}) {|hash, (key, val)| hash.merge(key.to_s => val) }
 
       # Raise if we can't parse the string, might be because of this bug :-(
       # https://bugs.ruby-lang.org/issues/12451
