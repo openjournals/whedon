d3b802e71c3ce0868ffef1a4808baafcb3f1e012
Boris Veytsman <borisv@lk.net>
2019-02-02 09:15:25 -0800

    Returned to plain_title in footers, but sanitized it

--- lib/whedon/processor.rb
+++ lib/whedon/processor.rb
@@ -99,6 +99,8 @@ module Whedon
 
       # TODO: Sanitize all the things!
       paper_title = paper.title.gsub!('_', '\_')
+      plain_title = paper.plain_title.gsub('_', '\_')
+      plain_title = paper.plain_title.gsub('#', '\#')
       paper_year ||= Time.now.strftime('%Y')
       paper_issue ||= @current_issue
       paper_volume ||= @current_volume
@@ -129,7 +131,7 @@ module Whedon
       -V formatted_doi="#{paper.formatted_doi}" \
       -V citation_author="#{paper.citation_author}" \
       -V paper_title='#{paper.title}' \
-      -V footnote_paper_title='#{paper.plain_title}' \
+      -V footnote_paper_title='#{plain_title}' \
       -o #{paper.filename_doi}.pdf -V geometry:margin=1in \
       --pdf-engine=xelatex \
       --filter pandoc-citeproc #{File.basename(paper.paper_path)} \
