2943d5a558c67d88d367d915a2df6a3b690d1397
Arfon Smith <arfon.smith@gmail.com>
2019-10-06 07:35:55 -0400

    Trying to fix Travis CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require "bundler/setup"
+require "fileutils"
 require "vcr"
 
 VCR.configure do |config|
@@ -8,3 +9,13 @@ VCR.configure do |config|
 end
 
 require_relative "../lib/whedon"
+
+# FIXME: This is kind of gross.
+# Rugged needs there to be a Git repo present in the fixtures folder when
+# running tests (spec/whedon_spec.rb#L11). Also, we're hard-coding the path
+# to tmp/#{review_issue_id} so we need a fixture there.
+FileUtils.rm_r('tmp/17')
+FileUtils.copy_entry('fixtures/paper', 'tmp/17')
+
+# Any git repo will do (using the one from Whedon)
+FileUtils.copy_entry('.git', 'tmp/17/.git')
